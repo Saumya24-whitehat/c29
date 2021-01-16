18cfaa7744fbf880d90bee2ff3148de34eaa6a01
Saumya24-whitehat <saumya2427130164@gmail.com>
2021-01-16 21:51:30 +1030

    Slingshot and Rubber band

--- Slingshot.js
+++ Slingshot.js
@@ -8,6 +8,12 @@ class SlingShot{
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.slingimg1=loadImage("sprites/sling1.png");
+        this.slingimg2=loadImage("sprites/sling2.png");
+        this.slingimg3=loadImage("sprites/sling3.png");
+
+
+
         World.add(world, this.sling);
     }
 
@@ -19,9 +25,24 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            push();
+            if(pointA.x<200){
+                strokeWeight(10);
+                stroke(67,30,5);
+                line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x-20, pointA.y,pointB.x+30,pointB.y);
+                image(this.slingimg3, pointA.x-30, pointA.y-10,15,30);
+            }else{
+                strokeWeight(6);
+                stroke(67,30,5);
+                line(pointA.x+20, pointA.y, pointB.x-10, pointB.y);
+                line(pointA.x+20, pointA.y,pointB.x+30,pointB.y);
+                image(this.slingimg3, pointA.x+20, pointA.y-10,15,30);
+            }
+            pop();
         }
+        image(this.slingimg1, 200, 20);
+        image(this.slingimg2, 170, 20);
     }
     
 }
